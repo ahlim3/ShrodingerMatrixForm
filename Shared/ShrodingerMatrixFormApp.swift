bf5d63413df3e2609c1475dbfdbe69b1d76ebb90
Anthony Lim <alim3@hawki.iit.edu>
2021-03-26 13:11:19 -0500

    Initial Commit

---
+++ Shared/ShrodingerMatrixFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShrodingerMatrixFormApp.swift
+//  Shared
+//
+//  Created by anthony lim on 3/26/21.
+//
+
+import SwiftUI
+
+@main
+struct ShrodingerMatrixFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
